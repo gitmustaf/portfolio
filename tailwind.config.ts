cedd6910a1a4d3ba6fbbc0ffbc6bfe93f6e7d303
mustaf <mustafiaaa@gmail.com>
2025-10-10 18:43:18 +0530

    chore: setup project with next ts and tailwind

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-    content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-    theme: { extend: {} },
-    plugins: [],
-  };
-export default config
