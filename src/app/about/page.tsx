a53c2606c3f35019bb55ff5fec6b61b22268bdd5
mustaf <mustafiaaa@gmail.com>
2026-01-19 15:46:39 +0530

    feat: added sidebar and required tabs

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">About</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "../components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="relative md:flex">
+          <Sidebar />
+          <main className="flex-1 p-4">{children}</main>
+        </div>
       </body>
     </html>
   );
